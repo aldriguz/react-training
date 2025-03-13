afbcb01dd6def6bc065fc63535b85fed0892594d
Kevin Alama <alama1992@gmail.com>
2025-03-12 21:56:49 -0500

    WIP tic tac toe react

--- 002_tic_tac_toe/src/components/Board.tsx
+++ 002_tic_tac_toe/src/components/Board.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react"
 import Square from "./Square"
 
-export default function Board(){
+default function Board(){
 
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i: number) {
 
-        if (squares[i])
+        if (squares[i] || calculateWinner(squares))
             return
 
         const nextSquares = squares.slice()
@@ -22,8 +22,17 @@ export default function Board(){
         setXIsNext(!xIsNext)
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -40,5 +49,40 @@ export default function Board(){
                 <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
         </>
+    )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+    }
+    }
+    return null;
+
+}
+
+export export function Game(){
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
     )
 }
